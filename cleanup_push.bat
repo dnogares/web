dedc00d9d43f8ef6ca23c79fce27541ee79e3002
dnogares <dnogares@gmail.com>
2026-01-31 11:54:01 +0100

    Clean up debug files

---
+++ cleanup_push.bat
@@ -0,0 +1,4 @@
+@echo off
+git add .
+git commit -m "Clean up debug files"
+git push origin main
--- debug_git.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git status > debug_git.log 2>&1
-git add . >> debug_git.log 2>&1
-git commit -m "Debug commit" >> debug_git.log 2>&1
-git push origin main >> debug_git.log 2>&1
